9a8fd00b84dcedc0092bd18933328380a73a36d1
Michael Ekeghe <36678934+techlinesolutions@users.noreply.github.com>
2020-03-24 02:32:05 +0100

    Update FizzBuzz.java
    optimized for new lines

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -18,19 +18,32 @@ public class FizzBuzz {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for (int i = 1; i <= 100; i++) {
-            output = "";
-            if (i % 3 == 0) {
-                output += "Fizz";
+        final String FIZZ = "Fizz";
+   final String BUZZ = "Buzz";
+    final String FIZZ_BUZZ = "FizzBuzz";
+        for (int number = 1; number <= n; number++) {
+
+                // If number is divisible by 15 ( 5 * 3)
+                if (number % 15 == 0) {
+                    System.out.printf("%s ", FIZZ_BUZZ);
+                                System.out.print("\n");
+
+                } else if (number % 3 == 0) { // If number is divisible by 3
+                    System.out.printf("%s ", FIZZ);
+                                                    System.out.print("\n");
+
+                } else if (number % 5 == 0) { // If number is divisible by 5
+                    System.out.printf("%s ", BUZZ);
+                                                    System.out.print("\n");
+
+                } else {
+                    System.out.printf("%d ", number);
+                                                    System.out.print("\n");
+
+                }
             }
-            if (i % 5 == 0) {
-                output += "Buzz";
-            }
-           else {
-                output = String.valueOf(i);
-            }
-            System.out.println(output);
-        }
+
     }
+
 
 }
